09a99857db279ae7b7f2f0494f0bcb175638b375
Carlos Beverina <carlosbeverina@gmail.com>
2022-06-05 12:28:46 -0300

    4ta entrega: se crea ItemCount y se implementa la logica

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemCount from './components/ItemCount';
 
 function App() {
+  
+  const onAdd = (count) => {
+    alert(`Agregaste ${count} items al carrito`)
+  }
+  
   return (
   <>
   <NavBar />
   <ItemListContainer greeting='Proximamente miles de productos para vos!!!' />
+  <ItemCount initial={1} stock={10} onAdd= {onAdd} />
   </>
   );
 }
---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { useState } from "react";
+import { Button, Col, Container, Row, Stack } from "react-bootstrap";
+
+function ItemCount({ initial, stock, onAdd }) {
+  const [count, setCount] = useState(initial);
+
+  const agregar = () => {
+    if (count < stock) {
+      setCount(count + 1);
+    } else {
+      alert("No se pueden agregar mas productos.");
+    }
+  };
+
+  const sacar = () => {
+    if (count > initial) {
+      setCount(count - 1);
+    } else {
+      alert("No se pueden sacar mas productos.");
+    }
+  };
+
+  const reset = () => {
+    setCount(initial);
+  }
+
+  return (
+    <>
+      <Container>
+          <Row>
+          <Col className="d-flex justify-content-center ">
+              <Button variant= "secondary" className="" onClick={agregar}>+</Button>
+              <h2 className="contador m-5">{count}</h2>
+              <Button variant= "secondary" onClick={sacar}>-</Button>
+          </Col>
+          </Row>
+          <Row>
+          <Col className="d-flex justify-content-center mt-2">
+            <div>
+              <Button variant="outline-secondary" onClick={() => {onAdd(count); reset()}}>Comprar</Button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+}
+
+export default ItemCount;
